829c68b4db4a9a7ee55f66b0926ee91f2379d97d
mohamed <mohamedyuossef979@gmail.com>
2026-05-12 23:46:06 +0300

    feat: use keycloak

--- src/main/java/com/shabic/api_gateway/SecurityConfiguration.java
+++ src/main/java/com/shabic/api_gateway/SecurityConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
@@ -15,16 +15,16 @@ public class SecurityConfiguration {
 
 	@Bean
 	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http,
-			ObjectProvider<JwtDecoder> jwtDecoder) {
+			ObjectProvider<ReactiveJwtDecoder> reactiveJwtDecoder) {
 		http.csrf(ServerHttpSecurity.CsrfSpec::disable);
 
-		JwtDecoder decoder = jwtDecoder.getIfAvailable();
+		ReactiveJwtDecoder decoder = reactiveJwtDecoder.getIfAvailable();
 		if (decoder != null) {
 			http.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
 		}
 
 		http.authorizeExchange(exchanges -> {
-			exchanges.pathMatchers("/api/animals", "/api/animals/**").permitAll();
+			exchanges.pathMatchers("/actuator/health", "/actuator/info").permitAll();
 			if (decoder != null) {
 				exchanges.anyExchange().authenticated();
 			} else {
