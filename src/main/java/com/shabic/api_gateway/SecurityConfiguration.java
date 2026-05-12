9fb7c96fea0cd8e18b4f418e71d6537ffaf7fd7f
mohamed <mohamedyuossef979@gmail.com>
2026-05-12 23:27:34 +0300

    add naming server config

---
+++ src/main/java/com/shabic/api_gateway/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package com.shabic.api_gateway;
+
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+
+	@Bean
+	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http,
+			ObjectProvider<JwtDecoder> jwtDecoder) {
+		http.csrf(ServerHttpSecurity.CsrfSpec::disable);
+
+		JwtDecoder decoder = jwtDecoder.getIfAvailable();
+		if (decoder != null) {
+			http.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+		}
+
+		http.authorizeExchange(exchanges -> {
+			exchanges.pathMatchers("/api/animals", "/api/animals/**").permitAll();
+			if (decoder != null) {
+				exchanges.anyExchange().authenticated();
+			} else {
+				exchanges.anyExchange().permitAll();
+			}
+		});
+
+		return http.build();
+	}
+}
